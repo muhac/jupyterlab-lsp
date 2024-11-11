6eb5eae6884ad96a0bfbc04e80dac00ccfecbd2a
Muhan Li <limuhan@msn.com>
2024-11-10 22:56:08 -0500

    upgrade unified language server

--- python_packages/jupyter_lsp/jupyter_lsp/specs/unified_language_server.py
+++ python_packages/jupyter_lsp/jupyter_lsp/specs/unified_language_server.py
@@ -2,16 +2,16 @@ from .utils import NodeModuleSpec
 
 
 class UnifiedLanguageServer(NodeModuleSpec):
-    node_module = key = "unified-language-server"
-    script = ["src", "server.js"]
-    args = ["--parser=remark-parse", "--stdio"]
+    node_module = key = "remark-language-server"
+    script = ["index.js"]
+    args = ["--stdio"]
     languages = ["markdown", "ipythongfm", "gfm"]
     spec = dict(
         display_name=key,
         mime_types=["text/x-gfm", "text/x-ipythongfm", "text/x-markdown"],
         urls=dict(
-            home="https://github.com/unifiedjs/{}".format(key),
-            issues="https://github.com/unifiedjs/{}/issues".format(key),
+            home="https://github.com/remarkjs/{}".format(key),
+            issues="https://github.com/remarkjs/{}/issues".format(key),
         ),
         install=dict(
             npm="npm install --save-dev {}".format(key),
